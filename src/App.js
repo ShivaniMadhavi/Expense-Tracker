5d6e1022579f6cd64a22f7f3d3f83e263f5e6326
Shivani Vinod Madhavi <shivani.madhavi@zee.com>
2021-07-27 17:56:32 +0530

    Expence tracker

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        amount: "",
+        totalamount:0,
+        transactions:[],
+        errorMsg:""
+    };
+  }
+
+  onAdd =()=>{
+    if(this.state.amount)
+    {
+      var data={
+        date:new Date().toISOString(),
+        amount:this.state.amount,
+        type:"Add"
+      }
+  
+      let  totalamount= Number(this.state.totalamount) + Number(this.state.amount)
+      this.setState(previousState => ({
+        transactions: [...previousState.transactions, data],
+        totalamount:totalamount,
+        amount:""
+      }));
+
+    }
+    else
+    {
+      this.setState({
+        errorMsg:"Please Enter Amount"
+      })
+    }
+  }
+
+  onRemove = () => {
+    if(this.state.amount)
+    {
+      var data={
+        date:new Date().toISOString(),
+        amount:this.state.amount,
+        type:"Remove"
+      }
+      let totalamount = Number(this.state.totalamount) - Number(this.state.amount)
+      this.setState(previousState => ({
+        transactions: [...previousState.transactions, data],
+        totalamount:totalamount,
+        amount:""
+      }));
+    }
+    else
+    {
+      this.setState({
+        errorMsg:"Please Enter Amount"
+      })
+
+    }
+  }
+
+  onChange =(e)=>{
+    this.setState({
+      amount:e.target.value,
+      errorMsg:""
+    })
+  }
+
+  render()
+  {
+    const {transactions}=this.state
+    const TransactionHistory = transactions.map((data,index) => <p key={index}>{data.date +" - "+data.amount+" - "+data.type}</p>);
+
+    return (
+      <div className="App">
+        <h1>Expense Tracker - Basic</h1>
+          <div id="outer-div">
+            <div id="inner-div">
+              <label>Balance : {this.state.totalamount}</label><br/>
+              <input type="number" name="amount" onChange={this.onChange} value={this.state.amount}/><br/>
+              <button onClick={this.onAdd}>Add</button> <button onClick={this.onRemove}>Remove</button><br/>
+              <label className="errorMsg">{this.state.errorMsg}</label>
+            </div>
+          </div>
+
+          <div id="outer-div">
+            <div  className="transactionHistory">
+              <b>Transactions : </b>
+              {TransactionHistory}
+            </div>
+          </div>
+        
+      </div>
+    );
+  }
+ 
 }
 
 export default App;
+
+
+
